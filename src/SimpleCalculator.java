91877f9d93bb7a939f4733ae5737dd8310454ce1
nguyenkhanhtung <tungnk0711@gmail.com>
2018-09-18 11:09:00 +0700

    commit project

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -1,10 +1,10 @@
 public class SimpleCalculator {
     public static int add(int first, int second){
-        throw new UnsupportedOperationException();
+        return first + second;
     }
 
     public static int sub(int first, int second){
-        throw new UnsupportedOperationException();
+        return first - second;
     }
 }
 
