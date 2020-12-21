7f8388ea49de892b4f03a037ec93104d639c2d1b
gio lang du <you@example.com>
2020-12-21 11:02:38 +0700

    Simple Calculator

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -1,6 +1,6 @@
 public class SimpleCalculator {
     public static int add(int first, int second){
-        return first + second;
+        return first+second;
     }
 
     public static int sub(int first, int second){
